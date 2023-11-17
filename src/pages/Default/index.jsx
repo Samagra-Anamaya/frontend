15b260155d3c71c4be7b67306392a17f57b2c0b7
amit-s19 <coutamit.s19@gmail.com>
2023-11-17 11:31:40 +0530

    Merge fix

--- src/pages/Default/index.jsx
+++ src/pages/Default/index.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import AssignedLocations from "../assigned-locations";
 import { useMachine } from '@xstate/react';
-import authMachine from "src/app/xstate/stateMachine";
+import authMachine from "../../xstate/stateMachine";
 import Home from "../Home";
 import { useSelector } from "react-redux";
 const Login = () => {
