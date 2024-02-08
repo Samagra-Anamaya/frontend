0ec95eb6f5c745b0b6a73383f749d100cfb44b07
amit-s19 <coutamit.s19@gmail.com>
2024-02-08 14:21:38 +0530

    Added pwa update notification

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -178,6 +178,35 @@ export default function App({ Component, pageProps, flagsmithState }) {
     }
   }, []);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
+      const wb = window.workbox
+
+      const promptNewVersionAvailable = event => {
+        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+        // You may want to customize the UI prompt accordingly.
+        if (confirm('A newer version of this web app is available, reload to update?')) {
+          wb.addEventListener('controlling', event => {
+            window.location.reload()
+          })
+
+          // Send a message to the waiting service worker, instructing it to activate.
+          wb.messageSkipWaiting()
+        } else {
+          console.log(
+            'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.'
+          )
+        }
+      }
+
+      wb.addEventListener('waiting', promptNewVersionAvailable)
+
+      // never forget to call register as auto register is turned off in next.config.js
+      wb.register()
+    }
+  }, [])
+
   return hydrated ? (
     <>
       {isDesktop ? (
