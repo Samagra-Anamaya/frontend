c50ed1a31e83c6a5fa657d40f5dd0b3392259f86
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2024-02-08 20:56:35 +0530

    feat: clear cache logic added

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   //disable: process.env.NODE_ENV === "development"
-  disable:false
+  disable:false,
+
+
 });
 
 module.exports = withPWA({
